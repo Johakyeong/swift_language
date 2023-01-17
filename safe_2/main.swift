a334a93410427178d993566576cb4277e0b79582
hakyeong <gkrud9124@naver.com>
2023-01-18 01:44:25 +0900

    2

--- safe_2/main.swift
+++ safe_2/main.swift
@@ -4,3 +4,30 @@ print("first commit")
 
 var a = 1
 print("a: \(a)")
+
+
+let b = 10
+var c = 5
+c = b
+print("c: \(c)")
+
+
+let (x, y) = (1, 2)
+print(x)
+print(y)
+
+
+let three = 3
+let minusThree = -three       // minusThree는 -3
+let plusThree = -minusThree   // plusThree는 3, 혹은 "minus minus 3"
+print(three)
+print(minusThree)
+print(plusThree)
+
+
+let name = "wdrld"
+if name == "world" {
+    print("hello, world")
+} else {
+    print("I'm sorry \(name), but I don't recognize you")
+}
